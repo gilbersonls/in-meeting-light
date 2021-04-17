a699abc927a0b36b23a9d2eb8f7e69c66a437cbb
Gilberson Silva <gilbersonls@gmailcom>
2021-04-17 19:39:02 -0300

    create react-native application without expo and makes a POC to older android phones

---
+++ InMeetingLightMobile/App.tsx
@@ -0,0 +1,204 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import BackgroundService from 'react-native-background-actions';
+import Sound from 'react-native-sound';
+
+import {IN_MEETING_LIGHT_API_URL} from '@env';
+import axios from 'axios';
+
+Sound.setCategory('Playback');
+
+var whoosh = new Sound('space.mp3', Sound.MAIN_BUNDLE, error => {
+  if (error) {
+    console.log('failed to load the sound', error);
+    return;
+  }
+
+  whoosh.setNumberOfLoops(-1);
+
+  // loaded successfully
+  console.log(
+    'duration in seconds: ' +
+      whoosh.getDuration() +
+      'number of channels: ' +
+      whoosh.getNumberOfChannels(),
+  );
+});
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const veryIntensiveTask = async () => {
+  // Example of an infinite loop task
+  await new Promise(async () => {
+    whoosh.play();
+
+    for (let i = 0; BackgroundService.isRunning(); i++) {
+      console.warn(i);
+
+      const {data: events} = await axios.get(
+        `${IN_MEETING_LIGHT_API_URL}/events`,
+      );
+
+      console.warn(events);
+
+      if (i === 14) {
+        whoosh.stop();
+      }
+      if (i === 20) {
+        whoosh.play();
+      }
+      await sleep(5000);
+    }
+  });
+};
+
+const options = {
+  taskName: 'Example',
+  taskTitle: 'ExampleTask title',
+  taskDesc: 'ExampleTask description',
+  taskIcon: {
+    name: 'ic_launcher',
+    type: 'mipmap',
+  },
+  color: '#ff00ff',
+  linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
+  parameters: {
+    delay: 1000,
+  },
+};
+
+const Section: React.FC<{
+  title: string;
+}> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Button
+            title="start"
+            onPress={() => BackgroundService.start(veryIntensiveTask, options)}
+          />
+          <Button
+            title="update"
+            onPress={() =>
+              BackgroundService.updateNotification({
+                taskDesc: 'New ExampleTask description',
+              })
+            }
+          />
+          <Button
+            title="stop"
+            onPress={() => {
+              whoosh.stop();
+              BackgroundService.stop();
+            }}
+          />
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ InMeetingLightMobile/env.d.ts
@@ -0,0 +1,3 @@
+declare module '@env' {
+  export const IN_MEETING_LIGHT_API_URL: string;
+}
